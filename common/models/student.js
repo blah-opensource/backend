6ffd1758df9596e3e4d286d30d5855260d3f5683
Kashif Minhaj <dailypctips@gmail.com>
2018-01-17 15:47:18 +0530

    Allow login with USN

--- common/models/student.js
+++ common/models/student.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Student) {
-
+    console.log("Student: create ", Student.create)
+    var origLogin = Student.login
+    Student.login = function(credentials, include, fn) {
+        origLogin = origLogin.bind(this);
+        if(credentials.usn) {
+            // set usn as username field
+            credentials.username = credentials.usn;
+            
+            origLogin(credentials, include, fn)
+        }
+    }
 };
